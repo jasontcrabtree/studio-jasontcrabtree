f645f0baee735815aff4efb4aeed4c996bd43a3a
jasontcrabtree <jasontcrabtree@gmail.com>
2024-10-20 16:18:34 +1300

    remove theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { GeistSans } from "geist/font/sans"
-import "@/ui/globals.css"
 import NavBar from "@/ui/components/nav-bar"
 import { useState } from "react"
-import ThemeProvider from "@/ui/components/theme-provider"
+
+import "@/ui/globals.css"
 
 export default function RootLayout({
     children,
@@ -14,22 +14,20 @@ export default function RootLayout({
     const [darkMode, setDarkMode] = useState(true)
 
     return (
-        <ThemeProvider>
-            <html lang="en" className={darkMode ? "dark" : ""}>
-                <head>
-                    <title>Home</title>
-                    <meta
-                        name="description"
-                        content="Having fun building new and interesting things"
-                    ></meta>
-                </head>
-                <body
-                    className={`${GeistSans.className} antialiased h-dvh bg-white dark:bg-black`}
-                >
-                    <NavBar theme={darkMode} themeHandler={setDarkMode} />
-                    <div className="w-full text-slate-800">{children}</div>
-                </body>
-            </html>
-        </ThemeProvider>
+        <html lang="en" className={darkMode ? "dark" : ""}>
+            <head>
+                <title>Home</title>
+                <meta
+                    name="description"
+                    content="Having fun building new and interesting things"
+                ></meta>
+            </head>
+            <body
+                className={`${GeistSans.className} antialiased h-dvh bg-white dark:bg-black`}
+            >
+                <NavBar theme={darkMode} themeHandler={setDarkMode} />
+                <div className="w-full text-slate-800">{children}</div>
+            </body>
+        </html>
     )
 }
